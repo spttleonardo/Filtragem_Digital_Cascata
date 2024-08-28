ec1b8976fe670a4a3abb3e47be2a802fafd132a3
Gabriel Mariano <gabrielmarianogsantos@gmail.com>
2024-08-28 13:18:36 -0300

    Commit gptolado, inserindo gráfico na janela

--- Filtro_teste.py
+++ Filtro_teste.py
@@ -4,9 +4,15 @@ import matplotlib.pyplot as plt
 import mplcursors
 import math
 from scipy.signal import lfilter, convolve
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg 
 
 import PySimpleGUI as sg
 
+def draw_plot(canvas, fig):
+    figure_canvas_agg = FigureCanvasTkAgg(fig, canvas)
+    figure_canvas_agg.draw()
+    figure_canvas_agg.get_tk_widget().pack(side='top', fill='both', expand=1)
+    return figure_canvas_agg
 
 def my_fft(sinal, fs):
     
@@ -101,91 +107,52 @@ def main(path):
     # Criando vetor de tempo
     tempo = np.arange(len(dados)) * 1e-3
 
-    plt.subplot(3, 1, 1)
-    plt.plot(tempo, dados)  # Adiciona marcadores de pontos
+    fig, ax = plt.subplots(3, 1, figsize=(10, 8))  # Criando a figura e os eixos
 
-    mplcursors.cursor(hover=True)
+    ax[0].plot(tempo, dados)
+    ax[0].set_title('Sinal Atual ao longo do tempo')
+    ax[0].set_xlabel('Tempo(s)')
+    ax[0].set_ylabel('Peso Atual')
+    ax[0].grid(True)
 
-    plt.title('Sinal Atual ao longo do tempo')
-    plt.xlabel('Tempo(s)')
-    plt.ylabel('Peso Atual')
-    plt.grid(True)
-    #plt.show()
-
-    # FFT
-    #my_fft(dados, 1000)
-
-    ### Iniciando projeto filtro
-
-    #frequencia de amostragem
+    # Filtro FIR
     fsamp = 1000
-
-    ## Definiçoes do filtro
-    fp = 35 # frequencia de passagem
-    fs = 50 # frequencia de corte
-
-    # normalizando em freq digital
-    wpd = 2*np.pi*fp
-    wsd = 2*np.pi*fs
-
-    wp = wpd/fsamp # frequencia de passagem digital
-    ws = wsd/fsamp # frequencia de corte digital
-
-    # frequencia de transicao
+    fp = 35
+    fs = 50
+    wpd = 2 * np.pi * fp
+    wsd = 2 * np.pi * fs
+    wp = wpd / fsamp
+    ws = wsd / fsamp
     wt = ws - wp
-
-    # frequencia de corte intermediaria
-    #wc = (ws + wp)/2
-
-    M = np.ceil((6.6*np.pi/wt))+1 #valor de 6.6 é tabelado para janela de hamming
-
-    hd = lowpass(ws,M) # funcao sinc para passa baixa ideal
-
-    # Aplicando a janela de Hamming
+    M = np.ceil((6.6 * np.pi / wt)) + 1
+    hd = lowpass(ws, M)
     w_hamm = np.hamming(M)
     h = hd * w_hamm
-
     dados_filtrado = convolve(dados, h)
-
     tempo_filtrado = np.arange(len(dados_filtrado)) * 1e-3
 
-    plt.subplot(3, 1, 2)
-    plt.plot(tempo_filtrado, dados_filtrado)
+    ax[1].plot(tempo_filtrado, dados_filtrado)
+    ax[1].set_title('Sinal Filtrado com FIR automatico')
+    ax[1].set_xlabel('Tempo(s)')
+    ax[1].set_ylabel('Amplitude')
+    ax[1].grid(True)
 
-    mplcursors.cursor(hover=True)
-
-    plt.title('Sinal Filtrado com FIR automatico')
-    plt.xlabel('Tempo(s)')
-    plt.ylabel('Amplitude')
-    plt.grid(True)
-    #plt.show()
-
-    # Filtro IIR first order manual
-    beta = 0.05
-    #IIR_manual(dados_filtrado, tempo_filtrado, beta)
-
-
-    # Filtro IIR first order auto
+    # Filtro IIR e Média Móvel
     beta = 0.1
     filtrado = IIR_auto(dados_filtrado, tempo_filtrado, beta)
-
-
     window_size = 55
-    window = np.ones(window_size)/window_size
+    window = np.ones(window_size) / window_size
     sinal_final = convolve(filtrado, window)
+    tempo_final = np.arange(len(sinal_final)) * 1e3
 
-    tempo_final = np.arange(len(sinal_final)) *1e3
+    ax[2].plot(tempo_final, sinal_final)
+    ax[2].set_title('Dados Filtrados e Média Móvel')
+    ax[2].set_xlabel('Tempo (s)')
+    ax[2].set_ylabel('Amplitude')
+    ax[2].grid(True)
 
-    plt.subplot(3, 1, 3)
-    plt.plot(tempo_final, sinal_final)
-
-    plt.legend()
-    plt.title('Dados Filtrados e Média Móvel')
-    plt.xlabel('Tempo (s)')
-    plt.ylabel('Amplitude')
-    plt.grid(True)
-    mplcursors.cursor(hover=True)
-    plt.show(block=False)
+    plt.tight_layout()
+    return fig  # Retornando o objeto Figure
 
 
 # Opções de filtro
@@ -201,12 +168,15 @@ layout = [
     [sg.Combo(options, key='-COMBO2-', default_value='Selecione a Opção')],
     [sg.Button('Confirmar'), sg.Button('Cancelar')],
     [sg.Text('Peso [g]: ')],
-    [sg.Text(key='-CAMPO-', enable_events=True)]
+    [sg.Text(key='-CAMPO-', enable_events=True)],
+    [sg.Canvas(key='-CANVAS-')]
 ]
 
 # Criação da janela
 window = sg.Window('Seleção de Arquivo', layout, size=(1200, 700))
 peso = 0
+# Inicialização do Canvas
+fig_canvas_agg = None
 
 # Loop de eventos
 while True:
@@ -222,7 +192,14 @@ while True:
         arquivo_selecionado = values['-FILE-']
         if arquivo_selecionado and opcao_selecionada != 'Selecione a Opção':
             #sg.popup(f'Você selecionou o arquivo: {arquivo_selecionado}')
-            main(arquivo_selecionado)
+            fig = main(arquivo_selecionado)
+            # Se já houver um gráfico no canvas, removê-lo
+            if fig_canvas_agg:
+                fig_canvas_agg.get_tk_widget().forget()
+            
+            # Desenhar o novo gráfico no Canvas
+            fig_canvas_agg = draw_plot(window['-CANVAS-'].TKCanvas, fig)
+
             peso = 280
             window['-CAMPO-'].update(peso)
         else:
