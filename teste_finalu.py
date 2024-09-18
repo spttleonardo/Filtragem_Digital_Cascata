0adedcd1eb4fb4fc9a0e26485595558ee6eaa770
Bruno Bueno Bronzeri <brunobronzeri@icloud.com>
2024-09-17 22:22:22 -0300

    comitando novas infos

--- teste_finalu.py
+++ teste_finalu.py
@@ -33,10 +33,9 @@ class UpdateableMatplotlibPlot:
 
     def figure_controller(self):
         if self.figure is None:
-            self.figure = plt.figure(figsize=(10, 8))
+            self.figure = plt.figure(figsize=(10, 8), facecolor='#558297')
             self.axes = self.figure.add_subplot(111)
             self.line, = self.axes.plot(self.data)
-            self.axes.set_title("Example of a Matplotlib plot updating in Tkinter")
         else:
             self.line.set_ydata(self.data)
             self.axes.relim()
@@ -114,6 +113,8 @@ def main(path, opt1, opt2, opt3):
     tempo = np.arange(len(dados)) * 1e-3
     fig, ax = plt.subplots(contador + 1, 1, figsize=(10, 8))
 
+    fig.tight_layout()
+
     ax[0].plot(tempo, dados)
     mplcursors.cursor(hover=True)
     ax[0].set_title('Sinal Atual ao longo do tempo')
@@ -126,9 +127,100 @@ def main(path, opt1, opt2, opt3):
             dados_filtrado = convolve(dados, h)
             tempo_filtrado = np.arange(len(dados_filtrado)) * 1e-3
             ax[1].plot(tempo_filtrado, dados_filtrado)
-            ax[1].set_title('Sinal Filtrado com FIR automático')
+            ax[1].set_title('Sinal Filtrado com FIR')
             ax[1].set_ylabel('Amplitude')
             ax[1].grid(True)
+        elif opt1 == 'Filtro IIR':
+            beta = 0.1
+            dados_filtrado = IIR_auto(dados,beta)
+            tempo_filtrado = np.arange(len(dados_filtrado)) * 1e-3
+            ax[1].plot(tempo_filtrado, dados_filtrado)
+            mplcursors.cursor(hover=True)
+            ax[1].set_title('Sinal Filtrado com IIR')
+            ax[1].set_ylabel('Amplitude')
+            ax[1].set_xticks([])
+            ax[1].grid(True)
+        elif opt1 == 'Média Móvel':
+            window_size = 55
+            window = np.ones(window_size) / window_size
+            dados_filtrado = convolve(dados, window)
+            tempo_filtrado = np.arange(len(dados_filtrado)) * 1e-3
+            ax[1].plot(tempo_filtrado, dados_filtrado)
+            mplcursors.cursor(hover=True)
+            ax[1].set_title('Sinal Filtrado com Média Móvel')
+            ax[1].set_ylabel('Amplitude')
+            ax[1].set_xticks([])
+            ax[1].grid(True)
+
+    if opt2 != 'Selecione a Opção':
+        if opt2 == 'Filtro FIR':
+            # Filtro FIR
+            h = filtro_fir()
+            dados_filtrado = convolve(dados_filtrado, h)
+            tempo_filtrado = np.arange(len(dados_filtrado)) * 1e-3
+
+            ax[2].plot(tempo_filtrado, dados_filtrado)
+            mplcursors.cursor(hover=True)
+            ax[2].set_title('Sinal Filtrado com FIR')
+            ax[2].set_ylabel('Amplitude')
+            ax[2].set_xticks([])
+            ax[2].grid(True)
+        elif opt2 == 'Filtro IIR':
+            beta = 0.1
+            dados_filtrado = IIR_auto(dados_filtrado,beta)
+            tempo_filtrado = np.arange(len(dados_filtrado)) * 1e-3
+            ax[2].plot(tempo_filtrado, dados_filtrado)
+            mplcursors.cursor(hover=True)
+            ax[2].set_title('Sinal Filtrado com IIR')
+            ax[2].set_ylabel('Amplitude')
+            ax[2].set_xticks([])
+            ax[2].grid(True)
+        elif opt2 == 'Média Móvel':
+            window_size = 55
+            window = np.ones(window_size) / window_size
+            dados_filtrado = convolve(dados_filtrado, window)
+            tempo_filtrado = np.arange(len(dados_filtrado)) * 1e-3
+            ax[2].plot(tempo_filtrado, dados_filtrado)
+            mplcursors.cursor(hover=True)
+            ax[2].set_title('Sinal Filtrado com Média Móvel')
+            ax[2].set_ylabel('Amplitude')
+            ax[2].set_xticks([])
+            ax[2].grid(True)
+
+    if opt3 != 'Selecione a Opção':
+        if opt3 == 'Filtro FIR':
+            # Filtro FIR
+            h = filtro_fir()
+            dados_filtrado = convolve(dados_filtrado, h)
+            tempo_filtrado = np.arange(len(dados_filtrado)) * 1e-3
+
+            ax[3].plot(tempo_filtrado, dados_filtrado)
+            mplcursors.cursor(hover=True)
+            ax[3].set_title('Sinal Filtrado com FIR')
+            ax[3].set_xlabel('Tempo(s)')
+            ax[3].set_ylabel('Amplitude')
+            ax[3].grid(True)
+        elif opt3 == 'Filtro IIR':
+            beta = 0.1
+            dados_filtrado = IIR_auto(dados_filtrado,beta)
+            tempo_filtrado = np.arange(len(dados_filtrado)) * 1e-3
+            ax[3].plot(tempo_filtrado, dados_filtrado)
+            mplcursors.cursor(hover=True)
+            ax[3].set_title('Sinal Filtrado com IIR')
+            ax[3].set_xlabel('Tempo(s)')
+            ax[3].set_ylabel('Amplitude')
+            ax[3].grid(True)
+        elif opt3 == 'Média Móvel':
+            window_size = 55
+            window = np.ones(window_size) / window_size
+            dados_filtrado = convolve(dados_filtrado, window)
+            tempo_filtrado = np.arange(len(dados_filtrado)) * 1e-3
+            ax[3].plot(tempo_filtrado, dados_filtrado)
+            mplcursors.cursor(hover=True)
+            ax[3].set_title('Sinal Filtrado com Média Móvel')
+            ax[3].set_xlabel('Tempo(s)')
+            ax[3].set_ylabel('Amplitude')
+            ax[3].grid(True)
 
     ax[contador-1].set_xlabel('Tempo(s)')
     return fig, dados_filtrado
@@ -221,58 +313,89 @@ def changeOnHover(button, colorOnHover, colorOnLeave):
 root = tk.Tk()
 root.title("Seleção de Arquivo")
 root.geometry("1500x1000")
+root.configure(bg="#436D80")
 
 # Frame esquerdo
-frame_left = tk.Frame(root)
-frame_left.pack(side=tk.LEFT, fill=tk.Y, padx=50, pady=50)
+frame_left = tk.Frame(root, bg="#436D80")
+frame_left.pack(side=tk.LEFT, fill=tk.Y, pady=50, padx=0)
+# frame_left.configure(bg="#436D80")
 
 # Frame direito
 frame_right = tk.Frame(root)
 frame_right.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
+# frame_right
 
-# Label e Entry para seleção de arquivo
-label_file = tk.Label(frame_left, text="Selecione um arquivo:")
-label_file.grid(row=0, column=0, padx=5, pady=5, sticky="w")
-entry_file = tk.Entry(frame_left, width=50)
-entry_file.grid(row=0, column=1, padx=5, pady=5, sticky="w")
-btn_browse = tk.Button(frame_left, text="Procurar", command=browse_file)
-btn_browse.grid(row=0, column=2, padx=5, pady=5)
+# Carrega logos
+logo_ufsc = tk.PhotoImage(file="./Images/logo_ufsc.png")
+logo_selgron = tk.PhotoImage(file="./Images/logo_selgron.png")
+
+# Label e Entry para seleção de arquivo ---------------------------------------------------------------------------
+label_file = tk.Label(frame_left, text="Arquivo:", font=('Ubuntu Medium', 15, 'bold'), bg="#436D80")
+label_file.grid(row=0, column=0, padx=10, pady=5, sticky="w")
+
+entry_file = tk.Entry(frame_left, width=25)
+entry_file.grid(row=0, column=0, padx=100, pady=5, sticky="w")
+entry_file.configure(font=('Arial', 12))
+
+btn_browse = tk.Button(frame_left, text="Procurar", command=browse_file, bg="lightgray")
+btn_browse.grid(row=1, column=0, columnspan=1, pady=30)
+btn_browse.configure(font=('Arial', 15, 'bold'))
+changeOnHover(btn_browse, "lightgreen", "lightgray")
 
 # Opções para combobox
 options = ["Filtro FIR", "Filtro IIR", "Média Móvel"]
 
 style = ttk.Style()
-style.configure('Custom.TCombobox',font=('Arial', 50), padding=15)
+# style.configure('Custom.TCombobox*Listbox', font=('Arial', 50), padding=15, width=0.01)
+root.option_add('*TCombobox*Listbox.font', ('Arial', 15))  # Ajusta a fonte das opções
 
-# Label e Combobox para Filtro 1
-label_opt1 = tk.Label(frame_left, text="Filtro 1:", font=('Ubuntu Medium', 15))
-label_opt1.grid(row=1, column=0, padx=5, pady=50, sticky="w")
+# Label e Combobox para Filtro 1 ---------------------------------------------------------------------------
+label_opt1 = tk.Label(frame_left, text="Filtro 1:", font=('Ubuntu Medium', 15, 'bold'), bg="#436D80")
+label_opt1.grid(row=2, column=0, padx=10, pady=50, sticky="w")
+
 combo_opt1 = ttk.Combobox(frame_left, values=options, state="readonly", style='Custom.TCombobox')
 combo_opt1.set("Selecione a Opção")
-combo_opt1.configure(font=('Arial', 12))
-combo_opt1.grid(row=1, column=1, padx=0, pady=50, sticky="ew")
+combo_opt1.configure(font=('Arial', 12, 'bold'))
+combo_opt1.grid(row=2, column=0, padx=100, pady=50, sticky="ew")
 
-# Label e Combobox para Filtro 2
-label_opt2 = tk.Label(frame_left, text="Filtro 2:")
-label_opt2.grid(row=2, column=0, padx=5, pady=50, sticky="w")
-combo_opt2 = ttk.Combobox(frame_left, values=options, state="readonly")
+# Label e Combobox para Filtro 2 ---------------------------------------------------------------------------
+label_opt2 = tk.Label(frame_left, text="Filtro 2:", font=('Ubuntu Medium', 15, 'bold'), bg="#436D80")
+label_opt2.grid(row=3, column=0, padx=10, pady=50, sticky="w")
+
+combo_opt2 = ttk.Combobox(frame_left, values=options, state="readonly", style='Custom.TCombobox')
 combo_opt2.set("Selecione a Opção")
-combo_opt2.grid(row=2, column=1, padx=5, pady=50, sticky="ew")
+combo_opt2.configure(font=('Arial', 12, 'bold'))
+combo_opt2.grid(row=3, column=0, padx=100, pady=50, sticky="ew")
 
-# Label e Combobox para Filtro 3
-label_opt3 = tk.Label(frame_left, text="Filtro 3:")
-label_opt3.grid(row=3, column=0, padx=5, pady=50, sticky="w")
-combo_opt3 = ttk.Combobox(frame_left, values=options, state="readonly")
+# Label e Combobox para Filtro 3 ---------------------------------------------------------------------------
+label_opt3 = tk.Label(frame_left, text="Filtro 3:", font=('Ubuntu Medium', 15, 'bold'), bg="#436D80")
+label_opt3.grid(row=4, column=0, padx=10, pady=50, sticky="w")
+
+combo_opt3 = ttk.Combobox(frame_left, values=options, state="readonly", style='Custom.TCombobox')
 combo_opt3.set("Selecione a Opção")
-combo_opt3.grid(row=3, column=1, padx=5, pady=50, sticky="ew")
+combo_opt3.configure(font=('Arial', 12, 'bold'))
+combo_opt3.grid(row=4, column=0, padx=100, pady=50, sticky="ew")
 
 # Label adicional
 # label_1 = tk.Label(frame_left, text="")
 # label_1.grid(row=4, column=0, columnspan=3, pady=30)
 
 # Botão de confirmação
-btn_confirm = tk.Button(frame_left, text="Confirmar", command=confirm_action)
-btn_confirm.grid(row=5, column=0, columnspan=3, pady=50)
+btn_confirm = tk.Button(frame_left, text="Confirmar", command=confirm_action, bg='lightgray')
+btn_confirm.grid(row=5, column=0, columnspan=1, pady=10)
+btn_confirm.configure(font=('Arial', 15, 'bold'))
+changeOnHover(btn_confirm, "lightgreen", "lightgray")
+
+# Reduz o tamanho da imagem pela metade (subsample aceita apenas inteiros)
+ufsc = logo_ufsc.subsample(7, 7)
+selgron = logo_selgron.subsample(2, 2)
+
+# Adiciona imagem no frame esquerdo
+label = tk.Label(frame_left, image=ufsc, bg="#436D80")
+label.grid(row=6, column=0, pady=40)
+
+label = tk.Label(frame_left, image=logo_selgron, bg="#436D80")
+label.grid(row=7, column=0, padx=0, pady=0)
 
 # Canvas no frame direito
 canvas = tk.Canvas(frame_right, bg="lightblue")
